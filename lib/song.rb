c5d636cc8d4f18d57dca47ae26310494eca5ce0f
Sharon Mun <sharonymun@gmail.com>
2018-09-06 01:34:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
     def initialize(name, artist, genre)
         @name = name
         @artist = artist
-        genre.add_artist(self)
+        
         @genre = genre
         genre.add_song(self)
         @@all << self
